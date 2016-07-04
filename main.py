80ea891868f7a821d9fee4232b3cc3b47f903f19
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-07-04 11:14:43 -0700

    confirmed working locally

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ import requests
 
 # microservices_urls = {
 #   'socket':'http://localhost:9000',
-#   'terrain': 'http://159.203.226.234',
-#   'field_objects': 'http://192.241.215.101', 
+#   'terrain': 'http://localhost:7000',
+#   'field_objects': 'http://localhost:7001', 
 # }
 microservices_urls = {
-    'socket': 'http://104.236.155.241',
+    'socket': 'http://104.236.155.241/',
     'terrain': 'http://159.203.226.234:7000',
     'field_objects': 'http://192.241.215.101:7001',
 }
