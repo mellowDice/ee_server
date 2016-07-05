cceb78d08b8f142108dd1f26dd333451f963707d
Austin Baltes <austinba@gmail.com>
2016-07-04 19:59:41 -0700

    Updated server to send separate socket events for landscape, terrain-objects, and player initialization

--- main.py
+++ main.py
@@ -24,138 +24,80 @@ app.config.from_envvar('APP_CONFIG_FILE')
 
 socketio = SocketIO(app, async_mode='eventlet')
 
-all_users = []
 user_count = 0
 food = {}
 obstacles = {}
-terrain = {}
 players = {}
 
 @app.route('/')
 def index():
     return 'Welcome to Ethereal Epoch'
 
-# @app.route('/send_terrain', methods=['POST'])
-# def terrain_creator(*args,  **kwargs):
-#     global terrain
-#     # print('terrain creator', request.json["terrain"])
-#     # print('response', r.json(), args['proxies'])
-#     terrain = request.json["terrain"]
-
-#     requests.get(app.config['OBJECTS_URL'] + '/terrain_objects')
-
-#     ### ADB: Send field objects to user
-#     return 'Ok'
-
+# Response from field-objects service
 @app.route('/send_field_objects', methods=['POST'])
 def field_object_creator(): 
-    global terrain
     food = request.json["food"]
     obstacles = request.json["obstacles"]
-    print('obstacles', obstacles)
-    print('food', food)
-    # print('terrain', terrain)
-    # print('Terrain called globally', list(terrain[:1]))
-
     socketio.emit('field_objects',
                   {'food': food, 
                    'obstacles': obstacles})
-    print('field_objects socket emit should have happened')
     return 'Ok'
 
-    
-# Helper Functions
-
-def create_location_object(user_id, data):
-    x = data["x"]
-    y = data["y"]
-    z = data["z"]
-    return {'id': user_id, 'x': x, 'y': y, 'z': z}
-
-def get_random_coordinate(height):
-    global terrain
-    coordinates = np.random.randint(height, size=2).tolist()
-    position = {'x': coordinates[0], 'y': coordinates[1], 'z': terrain[coordinates[0]][coordinates[1]]}
-    return position
-
+# Helper function to get all player information
 def get_all_players_on_start(): 
     for id in players:
         emit('requestPosition', {},  room=id)
         emit('spawn', {'id': id, 'mass': players[id]['mass']}, room=request.sid)
 
+# On new client connection, create food, obstacles, landscape, and inialize player (on new client and others)
 @socketio.on('connect')
-def test_connect():
-    global all_users, food, obstacles, microservices_urls, terrain
-    print('connect with socket info', request.sid)
+def on_connect():
+    print('NEW CONNECTION: ', request.sid)
+
+    # Request all player info to populate new player's screen
     get_all_players_on_start()
-    all_users.append(request.sid)
-    print(app.config)
-    # get_terrain(250, 250)
-    # Alert other users of new user and load data for game start
-    # print(food, obstacles)
-    # requests.get(microservices_urls["terrain"] + '/get_landscape', hooks=dict(response=terrain_creator))
-    response = requests.get(app.config['TERRAIN_URL'] + '/get_landscape')
-    terrain = response.json()
+
+    # Request for the terrain
+    terrain = requests.get(app.config['TERRAIN_URL'] + '/get_landscape').json()
     socketio.emit('landscape', {'terrain': list(terrain)}, room=request.sid)
-    # socketio.emit('landscape', {'terrain': list(terrain),
-    #               'food': food, 
-    #               'obstacles': obstacles}, broadcast=True)
-    # terrain = data
-    # print(data[0])
+
+    # Request for field-objects: Response dealt with above
     requests.get(app.config['OBJECTS_URL'] + '/terrain_objects')
 
+    # Initialize current player
     mass = DEFAULT_PLAYER_MASS * random.random()
+    players[request.sid] = {'mass': mass}
     socketio.emit('initialize_main_player',
                   {'id': request.sid, 
                    'mass': mass}, room=request.sid)
-    players[request.sid] = {'mass': mass}
+
+    # Spawn new player on other clients
     emit('spawn', {'id': request.sid, 'mass': mass}, broadcast=True, include_self=False)
-    # requests.get(app.config['']["terrain"] + '/get_landscape')
 
-    # # place items on the map (TODO: fix hardcoded height)
-    # height = 250
-    # for i in range(0, 15):
-    #     obstacles[i] = get_random_coordinate(height)
-    # for j in range(0, 100):
-    #     food[j] = get_random_coordinate(height)
-
-
-    # mass = DEFAULT_PLAYER_MASS * random.random()
-    # print("mass ",  mass)
-    # emit('load', {'id': request.sid, 'mass': mass, 'terrain': terrain, 'food': food, 'obstacles': obstacles}, room=request.sid)
-    # # emit('player_mass_update', {'id': request.sid, 'mass': mass})
-    # players[request.sid] = {'mass': mass}
-
-
-@socketio.on('move')
-def share_user_movement(json): 
-    # print('send user movement to other users' + str(json) + request.sid)
-    emit('playerMove', create_location_object(request.sid, json), broadcast=True, include_self=False)
-
-# @socketio.on('look')
-# def share_user_movement(json): 
-#     # print('send user movement to other users' + str(json) + request.sid)
-#     emit('otherPlayerLook',create_location_object(request.sid, json), broadcast=True, include_self=False)
+# Updates all clients when one client changes direction
 @socketio.on('look')
 def share_user_look_direction(json):
     # print('look: ' + request.sid)  
     emit('otherPlayerLook', dict({'id': request.sid}, **json), broadcast=True, include_self=False)
 
+# Updates all clients and reduces player mass when one client uses boost
 @socketio.on('boost')
 def share_user_boost_action(json):
     emit('otherPlayerBoost', {'id': request.sid}, broadcast=True, include_self=False);
     emit('playerMassUpdate', {'id': request.sid, 'mass': players[request.sid]['mass'] - DEFAULT_BOOST_COST})
 
+# Updates other players on player state in regular intervals
 @socketio.on('player_state_reconcile')
 def relay_player_state(json):
-    # print('state: ' + request.sid) 
     emit('otherPlayerStateInfo', dict({'id': request.sid}, **json), broadcast=True, include_self=False)
 
-@socketio.on('playerPosition')
-def send_position_to_new_user(json):
-    print('called this', json); 
-    print('this should really only go to new user', request.sid); 
-    # emit('updatePosition', create_location_object(request.sid, json), broadcast=True)
+# Message sent when a player is killed
+@socketio.on('kill_player')
+def kill(json): 
+    print('player killed', json)
+    emit('player_killed', {'id': json['id']}, broadcast=True, include_self=True)
+    ## Create food
+    ## Send Kill Player Signal
 
 @socketio.on('eat')
 def regenerate_food(json):
@@ -171,20 +113,12 @@ def regenerate_obstacle(json):
     obstacles[json.id]['id'] = json.id
     emit('collided', obstacles[json.id], broadcast=True)
 
-@socketio.on('kill_player')
-def kill(json): 
-    print('player killed', json)
-    emit('player_killed', {'id': json['id']}, broadcast=True, include_self=True)
-    ## Create food
-    ## Send Kill Player Signal
 
 # disconnect 
 
 @socketio.on('disconnect')
 def disconnect():
     print('Client disconnected', request.sid)
-    global all_users
-    all_users.remove(request.sid)
     emit('onEndSpawn', {'id': request.sid}, broadcast=True) # currently doens't de-render user
     del players[request.sid];
 
