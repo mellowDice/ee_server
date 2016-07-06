f280c7941dbfa756ec6a28bf8ba43bf1a075d06d
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-07-06 14:50:52 -0700

    Update redis with changes

--- main.py
+++ main.py
@@ -40,14 +40,14 @@ def index():
     return 'Welcome to Ethereal Epoch'
 
 # Response from field-objects service
-# @app.route('/send_field_objects', methods=['POST'])
-# def field_object_creator(): 
-#     food = request.json["food"]
-#     obstacles = request.json["obstacles"]
-#     socketio.emit('field_objects',
-#                   {'food': food, 
-#                    'obstacles': obstacles})
-#     return 'Ok'
+@app.route('/send_field_objects', methods=['POST'])
+def field_object_creator(): 
+    food = request.json["food"]
+    obstacles = request.json["obstacles"]
+    socketio.emit('field_objects',
+                  {'food': food, 
+                   'obstacles': obstacles})
+    return 'Ok'
 
 # Helper function to get all player information
 def get_all_players_on_start(): 
@@ -56,45 +56,47 @@ def get_all_players_on_start():
         clients[player['id']]
         emit('requestPosition', {},  room=player['id'])
         emit('spawn', {'id': player['id'], 'mass': player['mass']}, room=request.sid)
+
 # On new client connection, create food, obstacles, landscape, and inialize player (on new client and others)
 @socketio.on('connect')
 def on_connect():
-    global terrain
+    global terrain, players, clients
     print('NEW CONNECTION: ', request.sid)
 
     # Request for the terrain
+    print('terrain', terrain)
     if terrain == None:
-        terrain = requests.get(app.config['TERRAIN_URL'] + '/get_landscape')
+        terrain = requests.get(app.config['TERRAIN_URL'] + '/get_landscape').json()['result']
     # Note to self: make sure this returns in time on the first request
-    print('Terrain should exist before it emits', terrain)
-    socketio.emit('landscape', {'terrain': list(terrain)}, room=request.sid)
-
+    socketio.emit('load', {'terrain': list(terrain)}, room=request.sid)
     # Request for field-objects: Response dealt with above
     requests.get(app.config['OBJECTS_URL'] + '/terrain_objects')
 
     # Spawn all other players into new player's screen (Must happen before initializing current player)
-    players = requests.get(app.config['DB_URL'] + '/users/get_all')
-    print('players should exist', players)
-    # for player in players:
-    #     emit('spawn', {'id': player['id'], 'mass': player['mass']}, room=request.sid)
 
-    # Initialize current player
-    # mass = DEFAULT_PLAYER_MASS * (random.random() * 0.9 + 0.1)
-    # # players[request.sid] = {'mass': mass}
-    # # clients[request.sid] = {'zombies': []}
-    # playerPositionX = random.random() * (BOARD_WIDTH - 20) + 10
-    # playerPositionZ = random.random() * (BOARD_HEIGHT - 20) + 10
-    # requests.post(app.config['DB_URL'] + '/users/add', json={'mass': mass, 'id': request.sid, 'zombies': None})
-    # socketio.emit('initialize_main_player',
-    #               {'id': request.sid,
-    #                'mass': mass,
-    #                'x': playerPositionX,
-    #                'z': playerPositionZ}, room=request.sid)
-    # # Spawn new player on other clients
-    # emit('spawn', {'id': request.sid,
-    #                'mass': mass}, broadcast=True, include_self=False)
-    # # print('players count: ' + str(len(players)))
-    # add_more_zombies()
+    playersList = requests.get(app.config['DB_URL'] + '/users/get_all').json()
+
+    if playersList != None: 
+        for player in playersList:
+            print(player)
+            players[player['id']] = player
+            emit('spawn', {'id': player['id'], 'mass': player['mass']}, room=request.sid)
+    print('players should exist', players)
+
+    # players[request.sid] = {'mass': mass}
+    clients[request.sid] = {'zombies': []}
+    playerPositionX = random.random() * (BOARD_WIDTH - 20) + 10
+    playerPositionZ = random.random() * (BOARD_HEIGHT - 20) + 10
+    socketio.emit('initialize_main_player',
+                  {'id': request.sid,
+                   'mass': DEFAULT_PLAYER_MASS,
+                   'x': playerPositionX,
+                   'z': playerPositionZ}, room=request.sid)
+    # Spawn new player on other clients
+    emit('spawn', {'id': request.sid,
+                   'mass': DEFAULT_PLAYER_MASS}, broadcast=True, include_self=False)
+    # print('players count: ' + str(len(players)))
+    add_more_zombies()
 
 
 def add_more_zombies():
@@ -109,16 +111,21 @@ def add_more_zombies():
         current_zombie_id += 1
         zombiePositionX = random.random() * (BOARD_WIDTH - 20) + 10
         zombiePositionZ = random.random() * (BOARD_HEIGHT - 20) + 10
-        zombieID = 'zombie' + str(current_zombie_id)
+        zombieID = current_zombie_id
         socketio.emit('initialize_zombie_player',
                       {'id': zombieID, 
                        'mass': zombieMass,
                        'x': zombiePositionX,
                        'z': zombiePositionZ}, room=client_id_to_add_zombies_to)
-        players[zombieID] = {'mass': zombieMass}
+        players[zombieID]  = {'id': zombieID, 'mass': zombieMass}
         clients[client_id_to_add_zombies_to]['zombies'].append(zombieID)
-        emit('spawn', {'id': zombieID, 'mass': zombieMass}, broadcast=True, include_self=False)
-        print('finsihed spawning zombie ' + zombieID)
+        # Store zombie on assigned player 
+        requests.post(app.config['DB_URL'] + '/users/add', json={'mass': DEFAULT_PLAYER_MASS, 'id': client_id_to_add_zombies_to, 'zombies': zombieID})
+        # Store zombie as player
+        requests.post(app.config['DB_URL'] + '/users/add', json={'mass': zombieMass, 'id': zombieID, 'zombies': None })
+        
+        emit('spawn', {'id': zombieID, 'mass': zombieMass}, broadcast=True, include_self=False) 
+        print('finsihed spawning zombie ',  zombieID)
         print('players count: ' + str(len(players)))
 
 
@@ -134,10 +141,12 @@ def share_user_boost_action(json):
     emit('otherPlayerBoost', {'id': request.sid}, broadcast=True, include_self=False)
     player = requests.get(app.config['DB_URL'] + '/users/' + request.sid)
     emit('playerMassUpdate', {'id': request.sid, 'mass': player['mass'] - DEFAULT_BOOST_COST})
+    requests.post(app.config['DB_URL'] + '/users/add', json={'id': request.sid, 'mass': player['mass'] - DEFAULT_BOOST_COST })
 
 # Updates other players on player state in regular intervals
 @socketio.on('player_state_reconcile')
 def relay_player_state(json):
+    print('relay player state', json)
     emit('otherPlayerStateInfo', json, broadcast=True, include_self=False)
 
 # Message sent when a player is killed
@@ -147,12 +156,12 @@ def kill(json):
     print('player killed', json)
     emit('player_killed', {'id': id}, broadcast=True, include_self=True)
     players.pop(id, None)
+    requests.get(app.config['DB_URL'] + '/users/delete' + id)
     add_more_zombies()
 
 
     ## Create food
     ## Send Kill Player Signal
-    requests.post(app.config['DB_URL'] + '/users/end', json={'id': json['id']})
 
 @socketio.on('eat')
 def regenerate_food(json):
@@ -181,7 +190,6 @@ def disconnect():
     clients.pop(request.sid, None)
     add_more_zombies()
     print('players count: ' + str(len(players)))
-
 # error handling
 @socketio.on_error()    
 def error_handler(e):
